01674e986280fb52eb2b07fa21236f9c536272c6
StuartFleishman <Stuart.Alex.Fleishman@gmail.com>
2022-08-24 10:50:10 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Typography from "@material-ui/core/Typography";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Typography variant="h2" component="h2">
+          h1. Heading
+        </Typography>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
